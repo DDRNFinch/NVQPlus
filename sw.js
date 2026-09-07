878df0001013f6b1a9ec439d262cf88dde979ed8
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-09-07 22:36:10 +0100

    Cache complete NVQPlus PWA build

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-const CACHE='nvqplus-v2';
-const LOCAL=['./','./index.html','./app.css','./app.js','./course-data.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='nvqplus-v3';
+const LOCAL=['./','./index.html','./app.css','./app.js','./course-core.js','./unit-102.js','./units-234-235.js','./unit-238.js','./units-303-300-502.js','./unit-313.js','./unit-690.js','./unit-701.js','./unit-828.js','./unit-837.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 const REMOTE=[
   'https://cdn.jsdelivr.net/npm/pdf-lib@1.17.1/dist/pdf-lib.min.js',
-  'https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js'
+  'https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js',
+  'https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.js'
 ];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(LOCAL).then(()=>Promise.allSettled(REMOTE.map(u=>cache.add(u))))));
